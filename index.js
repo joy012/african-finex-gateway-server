39b93b0c8625bae43a2aba0bf1e78b1773c784c8
joy012 <joy.buet.012@gmail.com>
2021-01-29 02:53:53 +0600

    bug fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${precess.env.DB_PASS}@cluster0.ukskk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ukskk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
