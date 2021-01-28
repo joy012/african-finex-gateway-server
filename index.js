e690440cf709cc3612e402c8bcab2c4ac50d1c71
joy012 <joy.buet.012@gmail.com>
2021-01-29 00:24:47 +0600

    cors added

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId;
-var admin = require('firebase-admin');
+const admin = require('firebase-admin');
 const serviceAccount = require("./config/african-finex-gateway-firebase-adminsdk-m3aag-9a106308e1.json");
 require('dotenv').config()
 
